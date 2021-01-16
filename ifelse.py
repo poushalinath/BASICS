5d8ea2b75c308426558331a5374997f9ed534d6d
poushalinath <poushali002@gmail.com>
2021-01-16 15:47:00 +0530

    add a new file ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+price = 250
+if price >=  300:
+    print(price*0.7)
+elif price >= 200:
+    print(price*0.8)
+elif price >= 100:
+    print(price*0.9)
+elif price < 100:
+    print(price*0.95)
+else:
+    print("no discount")
