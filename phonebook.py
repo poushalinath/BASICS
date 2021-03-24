ee092b0ce4b4d0cdc64f62bc8561028bad644b7c
Poushali Nath <77223522+poushalinath@users.noreply.github.com>
2021-03-24 19:45:50 +0530

    update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,21 +1,29 @@
-numberb_of_entries = int(input(6))
+numberb_of_entries = int(input(10))
 print("Okay , give me name and number respectively for each of them in order")
-file = open("numbers.txt","w")
-for i in range (0,6):
+file = open("phonebook.txt","w")
+for i in range (0,10):
     name = input("Name: ")
     number = input("Number: ")
-    entry = f"{name}: {number},"
+    entry = f"{name}: {number} , "
     file.write(entry)
-    if i == 5:
+    if i == 9:
         print("Done!")
-    elif i == 4:
+    elif i == 8:
         print("Okay next please!")
+    elif i == 7:
+        print("Okay next please!")
+    elif i == 6:
+        print("Okay next please!")
+    elif i == 5:
+        print("Okay next please!")
+    elif i == 4:
+        print("Okay next please!") 
     elif i == 3:
         print("Okay next please!")
     elif i == 2:
-        print("Okay next please!")
+        print("Okay next please!") 
     elif i == 1:
-        print("Okay next please!")
+        print("Okay next please!")  
     else:
-        print("Okay next please!")
+         print("Okay next please!")
 file.close()
